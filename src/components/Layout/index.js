dc256e480f61e2537cd95f6ea72f403ab4636568
Douglas Dong <ad546971975@gmail.com>
2020-04-28 00:57:05 +0800

    add:添加黑夜模式

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import styled, { ThemeProvider } from "styled-components"
 
@@ -8,6 +8,7 @@ import { Box } from "../Box"
 
 import Global from "./global"
 import theme from "../../../theme"
+import ThemeButton from "../ThemeButton"
 
 const MainContainer = styled(Box)`
   padding: 40px ${props => props.theme.spacing["4"]};
@@ -21,25 +22,54 @@ const MainContainer = styled(Box)`
   }
 `
 
-const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <Global />
-    <MainContainer
-      backgroundColor="black"
-      display="flex"
-      element="main"
-      flexDirection="column"
-      fontFamily="default"
-      justifyContent="space-between"
-      minHeight="100vh"
-      textColor="silver"
-    >
-      <Navigation />
-      {children}
-      <Footer />
-    </MainContainer>
-  </ThemeProvider>
-)
+const Layout = ({ children }) => {
+  debugger;
+  const [isDark, setIsDark] = useState(localStorage.getItem('darkMode') === 'true');
+  // 如果非暗色模式的话，对其进行反色处理
+  const lightModeTheme = isDark ? {} : {
+    colors: {
+      black: {
+        lightest: "#bababa",
+        lighter: "#d5d5d5",
+        light: "#e9e9e9",
+        default: "#fff",
+      },
+      silver: {
+        default: "#787878",
+        darker: "#989898",
+        darkest: "#555",
+      },
+      white: {
+        default: "#111",
+      },
+    },
+  }
+  return (
+    <ThemeProvider theme={{
+      ...theme,
+      ...lightModeTheme,
+    }}>
+      <Global />
+      <MainContainer
+        backgroundColor="black"
+        display="flex"
+        element="main"
+        flexDirection="column"
+        fontFamily="default"
+        justifyContent="space-between"
+        minHeight="100vh"
+        textColor="silver"
+      >
+        <ThemeButton
+          onChange={setIsDark}
+        />
+        <Navigation />
+        {children}
+        <Footer />
+      </MainContainer>
+    </ThemeProvider>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
